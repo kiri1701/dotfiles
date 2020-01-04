68576bb88f41cd32b71f980d46d01214dc8a0836
Makoto Kirihata <kiri@MakotonoMacBook-Pro.local>
2020-01-04 12:05:05 +0900

    add mac_setup.sh

--- .zshrc
+++ .zshrc
@@ -36,33 +36,22 @@ alias vim='nvim'
 alias vi='vim'
 
 # 追加したソフトやパッケージ用のコマンドのパスを通す
-export PATH=/usr/local/bin:/Library/TeX/texbin:$PATH
 export PATH="$PATH:/usr/local/bin"
-export PATH="$HOME/.rbenv/bin:$PATH"
-export PATH=$PATH:/Library/Java/JavaVirtualMachines/jdk1.8.0_161.jdk/Contents/Home/bin
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_161.jdk/Contents/Home
-export PGDATA=/usr/local/var/postgres
+export PATH="/usr/local/bin:/Library/TeX/texbin:$PATH"
+export PATH="$PYENV_ROOT/bin:$PATH"
+export PATH=$HOME/.nodebrew/current/bin:$PATH
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 export EDITOR='vim' # nanoからvimに変更
 export VISUAL='vim' # nanoからvimに変更
-export PYENV_ROOT="${HOME}/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
+export PYENV_ROOT=/usr/local/var/pyenv
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
-export PATH=$HOME/.nodebrew/current/bin:$PATH
-export USERNAME='kiri1701'
+export PGDATA=/usr/local/var/postgres
+export PIPENV_VENV_IN_PROJECT=true
 
 # envのパスを通す
 ## Set path for pyenv
 if [ -d "${PYENV_ROOT}" ]; then
     export PATH=${PYENV_ROOT}/bin:$PATH
     eval "$(pyenv init -)"
-    eval "$(pyenv virtualenv-init -)"
 fi
-eval "$(pyenv virtualenv-init -)"
-eval "$(rbenv init -)"
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/kiri/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/kiri/google-cloud-sdk/path.zsh.inc'; fi
-# The next line enables shell command completion for gcloud.
-if [ -f '/Users/kiri/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/kiri/google-cloud-sdk/completion.zsh.inc'; fi
-
-export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
---
+++ mac_setup.sh
@@ -0,0 +1,49 @@
+# homebrew install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# basic install
+brew cask install alfred iterm2 visual-studio-code
+brew install openssl readline sqlite3 xz zlib pyenv direnv pipenv nodebrew tmux neovim mysql posgresql nginx peco fzf zsh-completions zsh-autosuggestions r docker
+brew cask install docker rstudio
+brew tap homebrew/cask-fonts && brew cask install font-fira-code
+
+# zsh setup
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+cp dotfiles/.zpreztorc .zprezto/runcomes/zpreztorc
+ln -s .zpreztorc .zprezto/runcoms/zpreztorc
+cp dotfiles/prompt_sorin_setup .zprezto/modules/prompt/functions/prompt_sorin_setup
+cp dotfiles/.tmux.conf ~/.tmux.conf
+
+# vim setup
+mkdir -p ~/.config/nvim
+cp dotfiles/.vimrc ~/.vimrc
+cp dotfiles/dein.toml ~/.config/nvim/dein.toml
+cp dotfiles/dein_lazy.toml ~/.config/nvim/dein_lazy.toml
+ln -s ~/.vimrc ~/.config/nvim/init.vim
+
+# python setup
+pyenv install 3.7.5
+pyenv global 3.7.5
+
+# node
+nodebrew setup
+nodebrew install-binary stable
+nodebrew use stable
+
+# latex setup
+brew cask install basictex latexit
+brew install ghostscript
+sudo tlmgr update --self --all
+sudo tlmgr paper a4
+sudo tlmgr install collection-langjapanese
+sudo tlmgr repository add http://contrib.texlive.info/current tlcontrib
+sudo tlmgr pinning add tlcontrib '*'
+sudo tlmgr install japanese-otf-nonfree japanese-otf-uptex-nonfree ptex-fontmaps-macos cjk-gs-integrate-macos
+sudo cjk-gs-integrate --link-texmf --cleanup
+sudo cjk-gs-integrate-macos --link-texmf
+sudo mktexlsr
+sudo kanji-config-updmap-sys --jis2004 hiragino-highsierra-pron
